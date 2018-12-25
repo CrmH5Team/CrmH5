94fe325a0516d6b873ce0388680b28b70c63b161
chenggengneng <253871127@qq.com>
2018-12-25 16:56:20 +0800

    列表页组件抽取
    Signed-off-by: chenggengneng <253871127@qq.com>

--- src/components/common/Commonlist.vue
+++ src/components/common/Commonlist.vue
@@ -1,14 +1,24 @@
 <!--  列表通用组件 -->
 <template>
-<div class="componetlist">
+<div>
+  <div v-if="groupData.length<=0" class="noDatabj"></div>
+  <div v-else id="page-content" class="page-content">
 
-    <div v-show="noData" class="noDatabj"></div>
-    <div v-show="!noData" id="refresh" class="refresh_box">
+        <div v-for="item in groupData" class="group-div">
+            <div class="date-div" @click="groupToggle($event)">
+                <span class="calcfont" :class="item.iconClass"></span>
+                <span class="group-name">{{item.groupName}}</span>
+                <span class="right">({{item.count}})</span>
+            </div>
+            <div class="occupy-div"></div>
+
+            <div class="group-item-list">
+                <slot></slot>
+            </div>
+        </div>
 
 
-
-    </div>
-
+  </div>
 </div>
 </template>
 
@@ -19,46 +29,41 @@
 export default {
   data (){
       return {
-
-          count : 1, //页码
-          Pagecount : 0, //总页数
-          SortOrder : '',
-          SortName : '',
-          QueryCondiction : [],
-
-          enablePullUpLoad: true, //是否可以滚动加载
-
-          rowData:[],  //列表数据
-          noData:false, //没有数据显示
-
-          isLoading:false, //滚动加载的状态(是否是正在加载中)
-          isLastPage:false, //是否加载到最后一页
-          isRefresh:false,  //是否禁止下来刷新功能
-
-          id:[],  //记录选择的id
-          componetlistHeight:0, //滚动列表父级的高度
-
-          scrollTop:0, //记录滚动条的位置
-
+          noData:false,
       }
   },
-  props: {
-      listData:Object
-  },
-
+  props: ['groupData'],
   created:function(){
-      this.scrollTop = 0;
+
   },
 
   mounted:function(){
+      lanTool.updateLanVersion();
 
-        lanTool.updateLanVersion();
-        var $this = this;
+      var headerH = parseFloat($('header').innerHeight());
+          $('#page-content').scroll(function(){
+              if($('.group-div').length <= 0) return ;
 
-        $this.$nextTick(function(){
-            $this.componetlistHeight = $('.componetlist').height();
-        })
+              $('.group-div').each(function(){
+                if($(this).position().top <= 0 ){
 
+                    if(tool.getSystem() === 'ios'){
+                        $(this).find(".date-div").addClass('sticky').css({"top": '0px'});
+                    }else{
+                        $(this).find('.date-div').css({"position":"fixed","top": headerH + 'px'});
+                        $(this).find('.occupy-div').show();
+                    }
+                }else{
+                    if(tool.getSystem() === 'ios'){
+                        $(this).find(".date-div").removeClass('sticky').css({"top":'0px'});
+                    }else{
+                        $(this).find('.date-div').css({"position":"static"});
+                        $(this).find('.occupy-div').hide();
+                    }
+                }
+
+              })
+          })
 
 
   },
@@ -66,16 +71,20 @@ export default {
   activated:function(){
       var $this = this;
 
-
-
-
   },
 
 
   methods:{
+      //点击分组收起展开
+      groupToggle:function(e){
+          var el = e.target;
 
-
-
+          if($(el).hasClass('open')){
+              $(el).removeClass('open').siblings('.group-item-list').hide(0.2);
+          }else{
+              $(el).addClass('open').siblings('.group-item-list').show(0.2);
+          }
+      },
 
   },
   deactivated:function(){
@@ -94,53 +103,29 @@ export default {
 
 
 <style scoped>
-.componetlist{/*height:calc(100% - 1.78rem);*/overflow: hidden;position: fixed;left:0;top:1.78rem;bottom:0;right:0;}
+.page-content{position:fixed;top:0.88rem;left:0;right:0;bottom:0;overflow-y:scroll;
+-webkit-overflow-scrolling:touch;}
+.date-div{
+  height:36px;line-height:36px;background:#ebf9fd;padding:0 10px;font-size: 12px;
+  width: 100%;color:#000;
+  box-sizing: border-box;
+  z-index: 99;
+}
+.item-stars-icon{display: inline-block;width: 0.6rem;height:0.6rem;line-height:0.6rem;text-align:center;position: absolute;top:8px;left:0.2rem;font-size:0.5rem;color:#ccc;}
+.calc-shoucang{color:#ff5a21}
+.date-div .calcfont{color:#ff5a21;margin-right:8px;}
+.group-name{font-size: 0.3rem;}
+.occupy-div{height:30px;display:none;}
+
+.group-item-list{display: none;}
+
+
+
 .noDatabj{
     background:#fff url(../../assets/images/nothing.png) no-repeat center 3.5rem;
     background-size: 2rem auto;
-    position:fixed;bottom:0;left:0;right:0;top:1.78rem;}
+    position:fixed;bottom:0;left:0;right:0;top:0.88rem;}
 
-.refresh_box{height:calc(100% + 50px);overflow-y: scroll;-webkit-overflow-scrolling:touch;}
- /* for Chrome */
-.refresh_box::-webkit-scrollbar {
-    display: none;
-}
-.weui-pull-to-refresh__layer div{font-size: 0.2rem;}
-
-/*列表 style*/
-.weui-cells{margin-top:0;}
-.weui-cells:after{border:0;}
-
-.weui-cell{position: relative;padding: 0 15px;}
-.weui-check__label:after {
-    content: " ";position: absolute;
-    left: 0;bottom: 0;right: 0;height: 1px;
-    border-bottom: 1px solid #7f7f7f;color: #7f7f7f;
-    -webkit-transform-origin: 0 100%;
-    transform-origin: 0 100%;
-    -webkit-transform: scaleY(0.5);
-    transform: scaleY(0.5);z-index: 2;
-}
-/*列表 style ----end-----*/
-
-.weui-loadmore{margin: 1.2em auto;}
-
-/*---- 底部style -----*/
-.EditBot {
-    position: fixed;z-index: 9; background:#efeff4; width: 100%;bottom: 0;
-    padding: 11px 15px;height: 50px;box-sizing: border-box;display: none;
-}
-.weui-check:checked + .weui-icon-checked:before{color:#3cadf9!important;}
-.EditBot .weui-check__label{position:relative;}
-.EditBot .weui-check__label:after {
-    border-bottom:none;
-    height: 0;
-}
-.EditBot a {
-    color: #fd3f3f;
-    font-size: 28px;
-    line-height: 28px;
-}
 </style>
 
 
