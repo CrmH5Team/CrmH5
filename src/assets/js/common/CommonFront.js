50640c974b1f362b8cc765dd98b0989275b12bc1
aoniruan <aoniwen@163.com>
2019-01-03 10:15:37 +0800

    更新icon链接

--- src/assets/js/common/CommonFront.js
+++ src/assets/js/common/CommonFront.js
@@ -2,7 +2,7 @@
 		var link = document.createElement('link');
 		link.setAttribute('type', 'text/css');
 		link.setAttribute('rel', 'stylesheet');
-		link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_pt45rvk11yq.css');
+		link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_9n9rdltlqjt.css');
 
 		//link.setAttribute('href', '../fonts/iconfont1.css');
     document.head.appendChild(link);
