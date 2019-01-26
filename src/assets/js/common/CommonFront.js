8784579307f6dd1037751b1efd39764b5396657c
aoniruan <aoniwen@163.com>
2019-01-26 11:52:10 +0800

    加icon

--- src/assets/js/common/CommonFront.js
+++ src/assets/js/common/CommonFront.js
@@ -2,7 +2,7 @@
 		var link = document.createElement('link');
 		link.setAttribute('type', 'text/css');
 		link.setAttribute('rel', 'stylesheet');
-		link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_o9hfg0hcqfp.css');
+		link.setAttribute('href', 'https://at.alicdn.com/t/font_651046_sd3gk3z40ge.css');
 
 		//link.setAttribute('href', '../fonts/iconfont1.css');
     document.head.appendChild(link);
