52ad4b32ee86cfd12d165268518c3ba2d3f960fa
dylan880507 <758777282@qq.com>
2019-01-28 18:18:19 +0800

    更新
    Signed-off-by: dylan880507 <758777282@qq.com>

--- src/components/pages/Contactsinfo.vue
+++ src/components/pages/Contactsinfo.vue
@@ -500,8 +500,18 @@ export default {
                     $('.ContactList,.accessList').removeClass('disable');
                 })
             }
-        }
+        },
+        //判断当前用户是否可以操作当前单据
+        initUserAccess:function(){
+            var _self = this;
+            var fromType = "6";
+            var fromID = _self.$route.params.id;
 
+            //是否指定记录的负责人
+            tool.IsHasInitiator(fromType,fromID,function(data){
+                _self.onlyView = data;
+            });
+        }
     }
 };
 </script>
