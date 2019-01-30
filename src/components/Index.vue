ab4d331cdf1251e831d96d3f40f4227a3bedafe5
chenggengneng <253871127@qq.com>
2019-01-30 18:47:06 +0800

    style
    Signed-off-by: chenggengneng <253871127@qq.com>

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -51,9 +51,11 @@
 
         <!-- 提示 -->
         <div class="tips">
-            <div class="tips-box">
+            <div class="tips-box f12">
                 <span class="calcfont calc-tishi1"></span>
-                <div class="tips-text f12"><span class="lanText" data-lanid="988_你有"></span><span>{{meetingCount}}</span><span class="lanText" data-lanid="987_场会议需要上传会议记录。"></span></div>
+                <div class="tips-text">
+                  <span class="lanText" data-lanid="988_你有"></span><span>{{meetingCount}}</span><span class="lanText" data-lanid="987_场会议需要上传会议记录。"></span>
+                </div>
                 <router-link v-show="Number(meetingCount)>=1" to="/meetinglist" class="right upload-now a lanText f14" data-lanid="870_立刻去上传"></router-link>
                 <!-- <div @click="" to="/meetinglist" class="right upload-now a lanText f14" data-lanid="870_立刻去上传"></div> -->
             </div>
