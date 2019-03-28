947f998266dc3662052f94b8792f9c60e9e82f7a
dylan880507 <758777282@qq.com>
2019-03-28 15:46:58 +0800

    更新删除
    Signed-off-by: dylan880507 <758777282@qq.com>

--- src/assets/js/common/common.js
+++ src/assets/js/common/common.js
@@ -3053,25 +3053,38 @@
 
 						//data = data._OnlyOneData || [];
 						//删除成功后跳转到列表页面
+						//兼容多个列表路由的情况，如business,businessCategories
 						var routeName = _self.$route.name;
 						var routers = _self.$router.options.routes;
-						var curRouter;
+						var curRouter = null;
+
+						//更改列表页面的FromSave标志
 						for(var i = 0;i<routers.length;i++){
-							if(routeName == routers[i].name){
+							//若是指定的
+							if(routers[i].name == routeName){
 								curRouter = routers[i];
 								break;
 							}
 						}
-						if(tool.isNullOrEmptyObject(curRouter)){
-							return;
-						}
-						routeName = curRouter.meta.listName;
-						for(var i = 0;i<routers.length;i++){
-							if(routeName == routers[i].name){
-								routers[i].meta.fromSave = true;
-								break;
+
+						if(!tool.isNullOrEmptyObject(curRouter)){
+							var listName = curRouter.meta.listName || "";
+							var listRouteNameArr = listName.split(',');
+							if(listRouteNameArr && listRouteNameArr.length >= 1){
+
+								for(var i = 0;i<routers.length;i++){
+									if($.inArray(routers[i].name,listRouteNameArr)>=0){
+										routers[i].meta.fromSave = true;
+										continue;
+									}
+								}
+
 							}
 						}
+
+
+
+
 						if (!tool.isNullOrEmptyObject(myCallBack)) {
 							myCallBack();
 						}
