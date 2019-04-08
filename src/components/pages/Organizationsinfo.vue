848710b328739901b3ef8b1b8b6d980c91e0d49b
dylan880507 <758777282@qq.com>
2019-04-08 14:08:33 +0800

    修改页面
    Signed-off-by: dylan880507 <758777282@qq.com>

--- src/components/pages/Organizationsinfo.vue
+++ src/components/pages/Organizationsinfo.vue
@@ -14,7 +14,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="ListCell visible">
+                <div class="ListCell">
                     <div class="ListCellLeftIcon textLeftIcon"><span class="mui-icon calcfont calc-CH"></span></div>
                     <div class="ListCellLeftText">
                         <p class="textareaP">
@@ -42,7 +42,7 @@
                             <div class="ListCellRightIcon"><span class="mui-icon calcfont calc-you"></span></div>
                         </div>
                     </div>
-                    <div class="ListCell visible">
+                    <div class="ListCell">
                         <div class="ListCellLeftIcon"><span class="mui-icon calcfont calc-nationaarea"></span></div>
                         <div class="ListCellContent">
                             <div class="ListCellContentLeft leftContent">
@@ -54,7 +54,7 @@
                             <div class="ListCellRightIcon"><span class="mui-icon calcfont calc-you"></span></div>
                         </div>
                     </div>
-                    <div class="ListCell visible">
+                    <div class="ListCell">
                         <div class="ListCellLeftIcon"><span class="mui-icon calcfont calc-diqiuquanqiu"></span></div>
                         <div class="ListCellContent">
                             <div class="ListCellContentLeft leftContent">
